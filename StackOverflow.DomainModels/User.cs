b1a3b0179ae2f909efb6da6f2b7fa75169cead32
Hibnachmu <37060153+Hibnachmu@users.noreply.github.com>
2023-05-12 15:58:06 +0200

    DomainModelClasses
    Added necessary classes representative of tables in sql db.

---
+++ StackOverflow.DomainModels/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflow.DomainModels
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string UserName { get; set; }
+        public string MobileNo { get; set; }
+        public string IsAdmin { get; set; }
+         
+    }
+}
