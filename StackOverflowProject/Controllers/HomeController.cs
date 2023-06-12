7c3e4fea0ef1871b506845c3627853c16771b75d
Hibnachmu <37060153+Hibnachmu@users.noreply.github.com>
2023-06-12 13:52:02 +0200

    dependencyInjection
    Added dependency injection for IQuestionServices.
    Made home page.

--- StackOverflowProject/Controllers/HomeController.cs
+++ StackOverflowProject/Controllers/HomeController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StackOverflowProject.ServiceLayer;
+using StackOverflowProject.ViewModels;
 
 namespace StackOverflowProject.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+
+        IQuestionsService qs;
+
+        public HomeController(QuestionsService qs)
+        {
+            this.qs = qs;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<QuestionViewModel> questions = this.qs.GetQuestions().Take(10).ToList();
+            return View(questions);
         }
     }
 }
